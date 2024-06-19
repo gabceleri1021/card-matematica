482e35a023f230da5971f0e71b0dda531fb0eabc
gab.celeri ∞ <Gabriel.Pedreiro.Senai404@gmail.com>
2024-06-19 11:47:52 -0300

    criacao do seletor de abas
    -criado seletor de abas
    -estilizado a pagina
    -perguntas inseridas

--- script/script.js
+++ script/script.js
@@ -1,7 +1,51 @@
+let abas = []
+let abaSelecionada
+
+const abaCards = {
+    btn: document.getElementById('btn-cards'),
+    aba: document.getElementById('aba-cards'),
+}
+const abaSobre = {
+    btn: document.getElementById('btn-sobre'),
+    aba: document.getElementById('aba-sobre')
+}
+
+//Adicionar abas no vetor para serem utilizados
+abas.push(abaCards)
+abas.push(abaSobre)
+
+function ativarAba(aba) {
+    abaSelecionada = aba
+    aba.btn.classList.add('seletor-btn-ativo')
+    aba.aba.classList.add('aba-selecionada')
+}
+
+function desativarAba(aba) {
+    aba.btn.classList.remove('seletor-btn-ativo')
+    aba.aba.classList.remove('aba-selecionada')
+}
+
+function selecionarAba(aba) {
+    desativarAba(abaSelecionada)
+    ativarAba(aba)
+    
+}
+
+//Selecionar uma aba por padrão para o código não quebrar
+ativarAba(abaCards)
+
+
+//Executar o código somente quando a página carregar
 document.addEventListener('DOMContentLoaded', (event) => {
     document.querySelectorAll('.card').forEach(card => {
         card.addEventListener('click', function () {
             card.classList.toggle('is-flipped');
         });
     });
+
+    abas.forEach(item => {
+        item.btn.addEventListener('click', function() {
+            selecionarAba(item)
+        })
+    })
 });
